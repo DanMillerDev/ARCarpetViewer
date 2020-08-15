d2a4dc2e894540a0f5b2020a0c24f5d3682c3119
Dan <danielmi@unity3d.com>
2020-08-15 15:29:42 -0700

    updated script to work on spawned planes

--- Assets/Carpet/Scripts/CarpetController.cs
+++ Assets/Carpet/Scripts/CarpetController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
 
 public class CarpetController : MonoBehaviour
 {
@@ -15,6 +16,13 @@ public class CarpetController : MonoBehaviour
     [SerializeField]
     Texture[] m_CarpetTextures;
 
+    [SerializeField]
+    ARPlaneManager m_PlaneManager;
+
+    Material[] m_PlaneMaterials;
+
+    Vector2 m_NewUVs;
+    
     void OnEnable()
     {
         m_UVSlider.onValueChanged.AddListener(delegate { UpdateUV();});
@@ -23,12 +31,24 @@ public class CarpetController : MonoBehaviour
 
     void UpdateUV()
     {
-        m_CarpetMat.mainTextureScale = new Vector2(m_UVSlider.value, m_UVSlider.value);
+        m_NewUVs =  new Vector2(m_UVSlider.value, m_UVSlider.value);
+
+        m_CarpetMat.mainTextureScale = m_NewUVs;
+
+        foreach (ARPlane plane in m_PlaneManager.trackables)
+        {
+            plane.GetComponent<MeshRenderer>().sharedMaterial.mainTextureScale = m_NewUVs;
+        }
     }
 
     public void SetCarpet(int arrayIndex)
     {
         m_CarpetMat.mainTexture = m_CarpetTextures[arrayIndex];
+        
+        foreach (ARPlane plane in m_PlaneManager.trackables)
+        {
+            plane.GetComponent<MeshRenderer>().sharedMaterial.mainTexture = m_CarpetTextures[arrayIndex];
+        }
     }
     
 }
