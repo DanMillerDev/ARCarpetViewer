2a20768d27dc10d5715d217e9289547bf1960993
DanMillerDev <danmiller126407@gmail.com>
2020-08-15 14:34:37 -0700

    initial commit

---
+++ Assets/Carpet/Scripts/CarpetController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CarpetController : MonoBehaviour
+{
+    [SerializeField]
+    Slider m_UVSlider;
+
+    [SerializeField]
+    Material m_CarpetMat;
+
+    [SerializeField]
+    Texture[] m_CarpetTextures;
+
+    void OnEnable()
+    {
+        m_UVSlider.onValueChanged.AddListener(delegate { UpdateUV();});
+        m_CarpetMat.mainTextureScale = new Vector2(m_UVSlider.value, m_UVSlider.value);
+    }
+
+    void UpdateUV()
+    {
+        m_CarpetMat.mainTextureScale = new Vector2(m_UVSlider.value, m_UVSlider.value);
+    }
+
+    public void SetCarpet(int arrayIndex)
+    {
+        m_CarpetMat.mainTexture = m_CarpetTextures[arrayIndex];
+    }
+    
+}
---
+++ Assets/Carpet/Scripts/SliderValue.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderValue : MonoBehaviour
+{
+    [SerializeField]
+    Slider m_UVSlider;
+
+    [SerializeField]
+    TMP_Text m_SliderDisplayValue;
+    
+    void OnEnable()
+    {
+        m_UVSlider.onValueChanged.AddListener(delegate {UpdateValueText();});
+        m_SliderDisplayValue.text = m_UVSlider.value.ToString();
+    }
+
+    void UpdateValueText()
+    {
+        m_SliderDisplayValue.text = m_UVSlider.value.ToString();
+    }
+    
+}
